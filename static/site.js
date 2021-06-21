c53660c5deab2f9863740fa41e2c3ac1d70e54b9
Rob Clancy <rotclanny@ksu.edu>
2021-06-21 05:39:23 +0000

    Added dynamic text abov images

--- static/site.js
+++ static/site.js
@@ -9,7 +9,13 @@ fetch('static/box-locations.json')
     let lat = boxArray[i].lat;
     let lng = boxArray[i].lng;
     console.log(boxArray[i].name);
-    mapdiv.appendChild(CreateElement(name, `https://open.mapquestapi.com/staticmap/v5/map?key=bHFGwnYDpeEqGPgYbqqmHBr7LuInUuNZ&locations=${lat},${lng}&size=200,200@2x`)); 
+    var myIMGDiv = document.createElement('div');
+    mapdiv.appendChild(myIMGDiv);
+    var p = document.createElement('p');
+    p.innerHTML = name;
+    var myMap = 
+    myIMGDiv.appendChild(p);
+    myIMGDiv.appendChild(CreateElement(name, `https://open.mapquestapi.com/staticmap/v5/map?key=bHFGwnYDpeEqGPgYbqqmHBr7LuInUuNZ&locations=${lat},${lng}&size=200,200@2x`)); 
   }
   
 });
@@ -22,13 +28,3 @@ function CreateElement(name, url) {
   return img;
 }
 
-window.onload = function() {
-  let mapdiv = document.getElementById("MapDiv");
-  for(let i = 0; i < boxArray.length; i++){
-    let name = boxArray[i].name;
-    let lat = boxArray[i].lat;
-    let lng = boxArray[i].lng;
-    console.log(boxArray[i].name);
-    //mapdiv.appendChild(CreateElement(name, `https://open.mapquestapi.com/staticmap/v5/map?key=bHFGwnYDpeEqGPgYbqqmHBr7LuInUuNZ&locations=${lat},${lng}&size=200,200@2x`)); 
-  }
-}
